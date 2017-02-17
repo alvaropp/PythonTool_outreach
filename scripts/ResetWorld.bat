3d44d95a9ecbecbab069bd1b601e0f591ff53b9a
AshleySetter <ajs3g11@soton.ac.uk>
2017-02-17 11:55:24 +0000

    ResetWorld script now resets all the scripts and adds to a counter of the number of times the script has been run

--- scripts/ResetWorld.bat
+++ scripts/ResetWorld.bat
@@ -1,2 +1,11 @@
 rmdir /S /Q saves\World1
+del /F /Q PythonToolScripts\*.*
 Xcopy /E /I BackUpWorld\World1 saves\World1
+Xcopy /E /I /y BackUpScripts\* PythonToolScripts\
+
+SET /P LASTNUMOFRUNS= < NumberOfRuns.dat
+SET /A NUMOFRUNS=%LASTNUMOFRUNS%+1
+echo %NUMOFRUNS%
+
+echo off
+> NumberOfRuns.dat echo %NUMOFRUNS%
