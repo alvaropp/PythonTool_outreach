a93552d1f3347d082c9fd398d08783bf04d6472e
Alvaro Perez <alvpdiaz@gmail.com>
2017-02-23 17:22:16 +0000

    added NA data for comparison

--- MinecraftOutreachData/DataAnalysis.py
+++ MinecraftOutreachData/DataAnalysis.py
@@ -17,6 +17,8 @@ EngagementScores = []
 for i, Entry in enumerate(EngagementScores_raw):
     if Entry in [0, 1, 2, 3, 4]:
         EngagementScores.append(Entry)
+    else:
+        EngagementScores.append(5)
 
 print(EngagementScores)
 print(len(EngagementScores))
@@ -24,8 +26,10 @@ print(len(EngagementScores))
 import matplotlib.pyplot as plt
 import numpy as np
 
-# the histogram of the data
-hist, bin_edges = np.histogram(EngagementScores, list(np.arange(-0.5, 5.5, 1)))
+# The histogram of the data
+hist, bin_edges = np.histogram(EngagementScores, list(np.arange(-0.5, 6.5, 1)))
+print(hist)
+print(bin_edges)
 
 width = 0.7 * (bin_edges[1] - bin_edges[0])
 center = (bin_edges[:-1] + bin_edges[1:]) / 2
@@ -33,8 +37,9 @@ center = (bin_edges[:-1] + bin_edges[1:]) / 2
 plt.xlabel('Engagement Score')
 plt.ylabel('Number of Kids')
 plt.title("Engagement Plot")
-plt.axis([-0.5, 4.5, 0, 15])
+plt.axis([-0.5, 5.5, 0, 15])
 plt.grid(True)
 plt.bar(center, hist, align='center', width=width)
-#plt.show()
+labels = ['0', '1', '2', '3', '4', 'NA']
+plt.xticks(center, labels)
 plt.savefig("EnagagementPlot.pdf")
