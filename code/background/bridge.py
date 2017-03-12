cd5ec7c15eda78f774255aedad06ba896ed816e6
Alvaro Perez <alvpdiaz@gmail.com>
2017-03-12 10:12:20 +0000

    Added bridge challende

---
+++ code/background/bridge.py
@@ -0,0 +1,27 @@
+#
+# Code by Alexander Pruss and under the MIT license
+#
+
+from mine import *
+import time
+import os
+
+def bridge(length):
+    mc = Minecraft()
+
+    bridge = []
+
+    while True:
+       pos = mc.player.getTilePos()
+       pos.y = pos.y - 1
+       belowBlock = mc.getBlock(pos)
+       if belowBlock == block.AIR.id or \
+               belowBlock == block.WATER_FLOWING.id or \
+               belowBlock == block.WATER_STATIONARY.id:
+         bridge.append(pos)
+         mc.setBlock(pos, block.STAINED_GLASS_BLUE)
+         if len(bridge) > length:
+             firstPos = bridge.pop(0)
+             if not firstPos in bridge:
+                 mc.setBlock(firstPos, block.AIR)
+       time.sleep(0.05)
